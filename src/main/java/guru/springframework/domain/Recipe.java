60b6cd8558c728ccc6f91dce0814f19ca4283d4b
xujie <jie.xu@thoughtworks.com>
2021-09-18 14:03:37 +0800

    spring framework guru, 134, @ManyToOne.

--- src/main/java/guru/springframework/domain/Recipe.java
+++ src/main/java/guru/springframework/domain/Recipe.java
@@ -6,7 +6,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.OneToMany;
 import javax.persistence.OneToOne;
+import java.util.Set;
 
 @Entity
 public class Recipe {
@@ -25,6 +27,9 @@ public class Recipe {
     private String directions;
 
     //todo: private Difficulty difficulty;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+    private Set<Ingredient> ingredients;
 
     @Lob
     private Byte[] image;
