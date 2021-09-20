3444ba231cdbc1da622f73a50447d396977bdb01
xujie <jie.xu@thoughtworks.com>
2021-09-20 10:25:24 +0800

    spring framework guru, 140, database-initialization.

--- src/main/java/guru/springframework/domain/Category.java
+++ src/main/java/guru/springframework/domain/Category.java
@@ -5,6 +5,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
+import javax.persistence.Table;
 import java.util.Set;
 
 @Entity
